31b84b968163746e06f8048472e1c9de5b45e546
atif9 <mohammadatif92080@gmail.com>
2021-02-06 10:54:31 +0530

    retrofit implied

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "62deaf18bc114a8ab749d5b3910b5fe1"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
